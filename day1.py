165bf14af4caeb2644887789f1e0be37767ade90
Job Muriithi <jobmuriithiandcompany@gmail.com>
2026-07-16 16:08:01 +0300

    Initial commit

---
+++ day1.py
@@ -0,0 +1,26 @@
+print ("Hello Word")
+
+# We have 4 DATA TYPES in Python
+
+# INTEGERS 1, 2 , 3, 20  WHOLE NUMBERS
+# STRINGS man , boy ,jobmuriithi83@gmail.com  SENTENCE
+# FLOAT  1.2, 2.4, 3.14 DECIMAL POINTS
+# BOOLEAN TRUE or FALSE 1, 0
+
+
+
+print ("My name is Job Muriithi and I am 20 years old")
+#VARIABLES
+# containers for data storage
+
+age = 20
+print (age)
+print ("My name is Job Muriithi and I am " + str(age) + " years old")
+print ("My name is Job Muriithi and I am " , age , "years old")
+print (f"My name is Job Muriithi and I am {age} years old")
+
+#SYNTAX
+if 5 > 10 :
+    print("FALSE")
+else :
+    print("TRUE")
