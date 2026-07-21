d9b4a84049ec301666d6ba7abd3bd6a143378a6b
Job Muriithi <jobmuriithiandcompany>
2026-07-21 20:52:26 +0300

    DAY 5 OF LEARNING PYTHON

---
+++ day5.py
@@ -0,0 +1,52 @@
+#INPUT , OUTPUT & TYPE CONVERSION
+
+#OUTPUT
+print("Hello, World!")
+print("Today is day 5 of learning python")
+
+#USER INPUT
+name = input("Enter your name: ")
+print ("What's up " + name)
+
+#F STRINGS
+#Easier to read
+#No need to convert data types manually
+#You can include operators + - * / 
+
+age = input("When were you born? ")
+print (f"You are {2026 - int(age)} years old")
+
+height = input("What is your height in meters? ")
+
+
+#ERRORS
+age = int(input("When were you born? "))
+
+print (age +10)
+
+#TYPE CONVERSION
+#This is changing the value from one data type to another
+
+age = 20
+print (float(age))
+
+#STRINGS AND INDEXING
+
+county = "Nairobi"
+
+print (county[0])
+print (county[1])
+print (county[-1])
+print (county[:4])
+
+name = "Job Muriithi"
+print (name.upper())
+print (name.lower())
+print (name.split())
+print (name.replace("Job", "JOBS"))
+print (name.strip())
+
+
+
+
+
